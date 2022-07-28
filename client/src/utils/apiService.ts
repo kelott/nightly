4fbe28ad9da795bd1e047ed1c52b14a737ce20bd
Kevin <kelott@gmx.de>
2022-07-28 12:02:48 +0200

    feat: add getOneProduct to apiService

--- client/src/utils/apiService.ts
+++ client/src/utils/apiService.ts
@@ -1,12 +1,21 @@
 // @ts-nocheck
 const baseUrl = 'https://fakestoreapi.com';
 const productsUrlEnd = '/products';
+const getHtml = { method: 'GET' };
 
 export async function getAllProducts() {
   try {
-    const response = await fetch(baseUrl + productsUrlEnd, {
-      method: 'GET',
-    });
+    const response = await fetch(baseUrl + productsUrlEnd, getHtml);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getOneProduct(productId: number) {
+  try {
+    const response = await fetch(baseUrl + productsUrlEnd + `/${productId}`, getHtml);
     const result = await response.json();
     return result;
   } catch (e) {
