93b585207876e67e6feb05ce3d478d51781d5664
Kevin <kelott@gmx.de>
2022-07-28 09:14:12 +0200

    fix: products type should be array of objects

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,12 +1,14 @@
-export type Products = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: Rating;
-};
+export type Products = [
+  {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: Rating;
+  }
+];
 
 export type Rating = {
   rate: number;
