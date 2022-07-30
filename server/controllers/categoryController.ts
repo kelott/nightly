7ce7468f9eaccb3c02086277ee9327da0e0fe94b
Kevin <kelott@gmx.de>
2022-07-30 16:40:28 +0200

    feat(backend): implement categoryController

--- server/controllers/categoryController.ts
+++ server/controllers/categoryController.ts
@@ -1,4 +1,26 @@
 // @ts-nocheck
 
-exports.getAllCategories = async (ctx) => {};
-exports.getProductsOfCategory = async (ctx) => {};
+exports.getAllCategories = async (ctx) => {
+  try {
+    ctx.body = await Category.findAll();
+    ctx.status = 200;
+  } catch (e) {
+    console.log(e);
+    ctx.status = 500;
+  }
+};
+exports.getProductsOfCategory = async (ctx) => {
+  const { categoryName } = ctx.params;
+
+  try {
+    ctx.body = await Category.findAll({
+      where: {
+        category: categoryName,
+      },
+    });
+    ctx.status = 200;
+  } catch (e) {
+    console.log(e);
+    ctx.status = 500;
+  }
+};
